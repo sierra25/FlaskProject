56df0596108225698bb7bafbc9f02c3fab5e65ec
sierra25 <lovejoy.sierra2@gmail.com>
2021-09-01 18:22:11 -0500

    Commit 09012021

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():  # put application's code here
-    return flask.render_template('intro.html')
+    return render_template("Intro.html");
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=False)
