e670ff7c7b0af3ccfd35eab7409475e90fc11ddf
sierra25 <lovejoy.sierra2@gmail.com>
2022-04-11 11:04:39 -0500

    Commit 09012021

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():  # put application's code here
-    return render_template("Intro.html");
+    return render_template("register.html");
 
 
 if __name__ == "__main__":
